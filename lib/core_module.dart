d6c2fbd0bdcefdc5ad10cb4ffe71bf302af5f869
Muhammad Noerhidayatullah <mnoer@outlook.com>
2021-05-24 14:09:15 +0700

    initial commit

---
+++ lib/core_module.dart
@@ -0,0 +1,6 @@
+library core_module;
+
+export 'package:core_module/src/error/exceptions.dart';
+export 'package:core_module/src/error/failures.dart';
+export 'package:core_module/src/usecase/usecase.dart';
+export 'package:core_module/src/util/observer.dart';
